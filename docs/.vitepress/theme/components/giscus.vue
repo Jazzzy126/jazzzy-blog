dd57bedb9a9aa0a4195e5c1685fd27064218b4e7
jiangzheyi <min126@qq.com>
2024-09-27 10:05:43 +0800

    feat(giscus): 评论
    新增底部评论功能

---
+++ docs/.vitepress/theme/components/giscus.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="giscus">
+    <Giscus
+        id="comments"
+        :key="route.path"
+        :theme="isDark ? 'dark' : 'light'"
+        category="General"
+        categoryid="DIC_kwDOM3zRBc4Ci1E9"
+        emitmetadata="0"
+        inputposition="top"
+        lang="zh-CN"
+        loading="lazy"
+        mapping="pathname"
+        reactionsenabled="1"
+        repo="jazzzy126/jazzzy-blog"
+        repoid="R_kgDOM3zRBQ"
+        term="Welcome to giscus!"
+    />
+  </div>
+</template>
+
+<script setup>
+import Giscus from '@giscus/vue';
+import {useData, useRoute} from 'vitepress';
+
+const route = useRoute();
+const {isDark} = useData();
+</script>
+
+<style scoped>
+.giscus {
+  margin-top: 24px
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,14 @@
 import Theme from 'vitepress/theme'
+import {h} from 'vue';
 import 'virtual:group-icons.css'
 import "./var.css"
+import Giscus from "./components/giscus.vue";
 
-export default Theme
+export default {
+    ...Theme,
+    Layout() {
+        return h(Theme.Layout, null, {
+            'doc-after': () => h(Giscus),
+        });
+    }
+}
